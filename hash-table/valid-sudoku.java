4c39f05fe2cced1b401c7c14978aecc01005e61d
Vasanthakumar <askvasanthkumar@gmail.com>
2026-07-26 19:21:32 +0530

    LC-valid-sudoku [Runtime: N/A; Memory: N/A]

---
+++ hash-table/valid-sudoku.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] != '.'){
+                    if(!seen.add(board[i][j] + "in row "+i) || !seen.add(board[i][j] + "in column "+j) || !seen.add(board[i][j] + "in block "+i/3+ "-" + j/3)) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
