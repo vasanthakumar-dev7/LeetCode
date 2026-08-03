6e4ba0da4e6227dfbd8ab85f367afd6251aaf2b7
Vasanthakumar <askvasanthkumar@gmail.com>
2026-08-03 15:46:12 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- sorting/merge-sorted-array.java
+++ sorting/merge-sorted-array.java
@@ -1,10 +1,36 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int j = 0;
-        for(int i=m; i<m+n; i++){
-            nums1[i] = nums2[j];
-            j++;
+        if(m==1 && n==0){
+            return;
+        } else if(m==0 && n==1){
+            nums1[0] = nums2[0];
         }
-        Arrays.sort(nums1);
+        int[] temp = new int[m+n];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                temp[k] = nums1[i];
+                i++;
+            }else{
+                temp[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<m){
+            temp[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while(j<n){
+            temp[k] = nums2[j];
+            j++;
+            k++;
+        }
+        for(int x=0; x<m+n; x++){
+            nums1[x] = temp[x];
+        }
     }
 }
