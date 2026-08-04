314cbdf6ef48adf18c99fdcad5413c8f2b28d147
Vasanthakumar <askvasanthkumar@gmail.com>
2026-08-04 16:15:41 +0530

    LC-find-missing-elements [Runtime: N/A; Memory: N/A]

--- hash-table/find-missing-elements.java
+++ hash-table/find-missing-elements.java
@@ -2,17 +2,14 @@ class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         List<Integer> res = new ArrayList<>();
         HashSet<Integer> set = new HashSet<>();
-        for(int num : nums) set.add(num);
+        for (int num : nums)
+            set.add(num);
+
+        Arrays.sort(nums);
         int mini = nums[0];
-        int maxi = nums[0];
-        for(int i=1; i<nums.length; i++){
-            mini = Math.min(mini, nums[i]);
-            maxi = Math.max(maxi, nums[i]);
-        }
-        System.out.println(mini);
-        System.out.println(maxi);
-        for(int i=mini; i<maxi; i++){
-            if(!set.contains(i)){
+        int maxi = nums[nums.length-1];
+        for (int i = mini; i < maxi; i++) {
+            if (!set.contains(i)) {
                 res.add(i);
             }
         }
