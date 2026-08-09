9263199f55930e57e9e61db9166e6d0a270b283d
Vasanthakumar <askvasanthkumar@gmail.com>
2026-08-09 08:43:01 +0530

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

---
+++ hash-table/subarray-sum-equals-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int c = 0;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        int presum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            presum += nums[i];
+            if (presum == k)
+                c++;
+            int rem = presum - k;
+            if (freq.containsKey(rem)) {
+                c++;
+            }
+            freq.put(presum, i);
+        }
+        return c;
+    }
+}
