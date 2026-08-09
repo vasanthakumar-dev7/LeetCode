7e7e843b62e524baf43665b1b15f42c124209fe2
Vasanthakumar <askvasanthkumar@gmail.com>
2026-08-09 09:24:25 +0530

    LC-find-the-power-of-k-size-subarrays-i [Runtime: N/A; Memory: N/A]

--- sliding-window/find-the-power-of-k-size-subarrays-i.java
+++ sliding-window/find-the-power-of-k-size-subarrays-i.java
@@ -1,27 +1,31 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        if(nums.length == 1 || k==1){
+        if (nums.length == 1 || k == 1) {
             return nums;
         }
-        int[] res = new int[nums.length-k+1];
-        for(int i=0; i<=nums.length-k; i++){
-            boolean flag = false;
-            int max = Integer.MIN_VALUE;
-            for(int j=i; j<i+k-1; j++){
-                if(nums[j]<nums[j+1] && nums[j+1] == nums[j]+1){
-                    flag = true;
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i <= nums.length - k; i++) {
+            if (checker(nums, i, k)) {
+                int max = Integer.MIN_VALUE;
+
+                for (int j = i; j < i + k; j++) {
                     max = Math.max(nums[j], max);
-                    max = Math.max(nums[j+1], max);
-                }else{
-                    flag = false;
                 }
-            }
-            if(flag){
                 res[i] = max;
-            }else{
+            } else {
                 res[i] = -1;
             }
         }
         return res;
+    }
+
+    public boolean checker(int[] nums, int i, int k) {
+        for (int j = i; j < i + k - 1; j++) {
+            if (nums[j] < nums[j + 1] && nums[j + 1] == nums[j] + 1) {
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
