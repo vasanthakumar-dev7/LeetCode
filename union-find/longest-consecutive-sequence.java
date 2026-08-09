b50499e823d5b1f77f71bd9e0ab3c40827d768f1
Vasanthakumar <askvasanthkumar@gmail.com>
2026-08-09 12:44:09 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

---
+++ union-find/longest-consecutive-sequence.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length <=1){
+            return nums.length0;
+        }
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for(int num : nums) freq.put(num, 0);
+        int maxi = 0;
+        for(int element : freq.keySet()){
+            int count = 0;
+            if(!freq.containsKey(element-1)){
+                while(freq.containsKey(element+1)){
+                    element = element+1;
+                    count++;
+                }
+            }
+            maxi = Math.max(count+1, maxi);
+        }
+        return maxi;
+    }
+}
